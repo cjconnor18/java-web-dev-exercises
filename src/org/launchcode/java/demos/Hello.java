f22a456e96a48948cfe9d6626495670a46583193
cjconnor18 <cjallen1988@gmail.com>
2021-08-24 13:13:53 -0500

    added code to help process running in intelliJ. Updated project structure settings

--- src/org/launchcode/java/demos/Hello.java
+++ src/org/launchcode/java/demos/Hello.java
@@ -8,3 +8,4 @@ public class Hello {
         System.out.println("Hello, World");
     }
 }
+/* had to change things in file-> project structure -> SDK version to 11 and project level language to 11 */
