95c4b46ab57ce5a0b2c86e9baf86fe41541fe232
cjconnor18 <cjallen1988@gmail.com>
2021-08-24 14:10:56 -0500

    Completed studio 1

---
+++ src/studios/studio1/Area.java
@@ -0,0 +1,18 @@
+package studios.studio1;
+
+import java.util.Scanner;
+
+public class Area {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("What is the radius of the circle?");
+        Double radius = input.nextDouble();
+
+        Double area = Circle.getArea(radius);
+
+        System.out.println("The area of the circle is " + area);
+
+        input.close();
+    }
+}
---
+++ src/studios/studio1/Circle.java
@@ -0,0 +1,7 @@
+package studios.studio1;
+
+public class Circle {
+    public static Double getArea(Double radius) {
+        return Math.PI * radius * radius;
+    }
+}
